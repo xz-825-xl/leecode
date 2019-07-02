cc80b897b87f341d29c9148d1b1a5e2ad7312851
zy <13216120922@163.com>
2019-07-02 17:15:30 +0800

    更新

--- src/tree/LevelOrderBottom.java
+++ src/tree/LevelOrderBottom.java
@@ -3,6 +3,7 @@ package tree;
 import entity.TreeNode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -14,7 +15,20 @@ public class LevelOrderBottom {
 
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> list = new ArrayList<>();
-
+        levelOrder(root, list, 0);
+        Collections.reverse(list);
         return list;
+    }
+
+    public void levelOrder(TreeNode root, List<List<Integer>> list, int level){
+        if(root != null){
+            if(level > list.size() - 1){
+                List<Integer> nodes = new ArrayList<>();
+                list.add(nodes);
+            }
+            list.get(level).add(root.val);
+            levelOrder(root.left, list, level + 1);
+            levelOrder(root.right, list, level + 1);
+        }
     }
 }
