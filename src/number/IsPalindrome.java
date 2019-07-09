bae9ee600cb6c860b01faab62fb9195e42611acf
zy <13216120922@163.com>
2019-07-09 11:20:28 +0800

    回文数

---
+++ src/number/IsPalindrome.java
@@ -0,0 +1,28 @@
+package number;
+
+/**
+ * @author Zhangying
+ * @date 2019/7/9
+ */
+public class IsPalindrome {
+
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+
+    public boolean isPalindrome2(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int reverseNum = 0;
+        int tmp = x;
+        while (tmp > 0){
+            reverseNum = reverseNum * 10 + tmp % 10;
+            tmp = tmp / 10;
+        }
+        return x == reverseNum;
+    }
+
+
+}
