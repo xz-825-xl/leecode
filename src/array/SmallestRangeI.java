351923d5494f85a2bc43ed172bcdac3fab754d5d
xz-825-xl <13216120922@163.com>
2019-08-31 11:23:50 +0800

    路径总和

--- src/array/SmallestRangeI.java
+++ src/array/SmallestRangeI.java
@@ -15,4 +15,16 @@ public class SmallestRangeI {
         return dis > 2 * K ? dis - 2 * K : 0;
     }
 
+    public int smallestRangeI2(int[] A, int K) {
+        if (A.length == 0) {
+            return 0;
+        }
+        int max = A[0];
+        int min = A[0];
+        for (int i = 1; i < A.length; i++) {
+            max = Math.max(max, A[i]);
+            min = Math.min(min, A[i]);
+        }
+        return Math.max((max - min) - 2 * K, 0);
+    }
 }
