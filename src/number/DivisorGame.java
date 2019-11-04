fcd947a1d286ca3421879447a32fc8382ba82ed4
zy <13216120922@163.com>
2019-11-04 09:22:50 +0800

    除数博弈

---
+++ src/number/DivisorGame.java
@@ -0,0 +1,13 @@
+package number;
+
+/**
+ * 描述：1025. 除数博弈
+ *
+ * @author zy
+ * @date 2019/11/4 8:46
+ */
+public class DivisorGame {
+    public boolean divisorGame(int N) {
+        return N % 2 == 0;
+    }
+}
