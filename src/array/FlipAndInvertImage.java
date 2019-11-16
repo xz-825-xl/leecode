79cea7c305e80ab929226fdd335af4f63540520f
zy <13216120922@163.com>
2019-11-16 13:15:41 +0800

    翻转图像

--- src/array/FlipAndInvertImage.java
+++ src/array/FlipAndInvertImage.java
@@ -20,4 +20,18 @@ public class FlipAndInvertImage {
         }
         return A;
     }
+
+    public int[][] flipAndInvertImage2(int[][] A) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length/2; j++) {
+                int temp = A[i][j];
+                A[i][j] = (A[i][A.length - 1 - j] ^ 1);
+                A[i][A.length - 1 - j] = (temp ^ 1);
+            }
+            if( A[i].length % 2 != 0){
+                A[i][A[i].length/2] ^= 1;
+            }
+        }
+        return A;
+    }
 }
