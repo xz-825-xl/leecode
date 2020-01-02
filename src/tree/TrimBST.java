5444049044f42305d66368cbc01b533590ba72d9
zy <13216120922@163.com>
2020-01-02 17:10:07 +0800

    修剪二叉搜索树

---
+++ src/tree/TrimBST.java
@@ -0,0 +1,27 @@
+package tree;
+
+import entity.TreeNode;
+
+/**
+ * 描述：669. 修剪二叉搜索树
+ *
+ * @author zy
+ * @date 2020/1/2 9:13
+ */
+public class TrimBST {
+
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val < L) {
+            return trimBST(root.right, L, R);
+        } else if (root.val > R) {
+            return trimBST(root.left, L, R);
+        } else {
+            root.left = trimBST(root.left, L, R);
+            root.right = trimBST(root.right, L, R);
+            return root;
+        }
+    }
+}
