dbb0680c819172aef7dc8ed0ce1add4ced883350
zy <13216120922@163.com>
2020-03-10 17:15:38 +0800

    有效的括号字符串

--- src/str/CheckValidString.java
+++ src/str/CheckValidString.java
@@ -1,5 +1,7 @@
 package str;
 
+import java.util.Stack;
+
 /**
  * 描述：678. 有效的括号字符串
  *
@@ -8,10 +10,32 @@ package str;
  */
 public class CheckValidString {
     public boolean checkValidString(String s) {
-        char[] chars = s.toCharArray();
-        for (int i = 0; i < chars.length; i++) {
+        Stack<Integer> left = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left.push(i);
+            } else if (s.charAt(i) == '*') {
+                star.push(i);
+            } else {
+                if (left.empty() && star.empty()){
+                    return false;
+                }
+                if (!left.empty()){
+                    left.pop();
+                } else{
+                    star.pop();
+                }
 
+            }
         }
-        return true;
+        while (!left.empty() && !star.empty()) {
+            if (left.peek() > star.peek()){
+                return false;
+            }
+            left.pop();
+            star.pop();
+        }
+        return left.empty();
     }
 }
