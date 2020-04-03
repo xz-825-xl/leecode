496582c492c7f58bfb42b56e44dc9bd260d0fb9b
zy <13216120922@163.com>
2020-04-03 18:23:46 +0800

    findTargetSumWays2

--- src/number/FindTargetSumWays.java
+++ src/number/FindTargetSumWays.java
@@ -9,6 +9,7 @@ package number;
 public class FindTargetSumWays {
 
     public static int findTargetSumWays(int[] nums, int S) {
+
         return findTargetSumWays(nums, S, 0);
     }
 
@@ -17,6 +18,23 @@ public class FindTargetSumWays {
             return S == 0 ? 1 : 0;
         }
         return findTargetSumWays(nums, S - nums[index], index + 1) + findTargetSumWays(nums, S + nums[index], index + 1);
+    }
+
+    public static int findTargetSumWays2(int[] nums, int S) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        int x = (S + sum) / 2;
+        int[][] rs = new int[nums.length + 1][x + 1];
+        rs[0][0] = 1;
+        for (int i = 1; i < nums.length + 1; i++) {
+            rs[i][0] = 1;
+            for (int j = 0; j < x + 1; j++) {
+
+            }
+        }
+        return rs[nums.length + 1][x + 1];
     }
 
     public static void main(String[] args) {
