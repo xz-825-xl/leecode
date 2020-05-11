a7e4ff7a606b04d696ee2cdfc0c41fe8f32dc100
zy <13216120922@163.com>
2020-05-11 19:16:37 +0800

    最短的桥

--- src/array/ShortestBridge.java
+++ src/array/ShortestBridge.java
@@ -7,4 +7,17 @@ package array;
  * @date 2020/5/9 16:51
  */
 public class ShortestBridge {
+
+    public int shortestBridge(int[][] A) {
+        int count = 1;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 1; j < A[i].length; j++) {
+                if (A[i][j - 1] > 0) {
+
+                }
+            }
+        }
+        return count;
+    }
+
 }
