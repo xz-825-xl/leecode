767aeab26d2a66cbaa57556c7f62d99d46a3e676
zy <13216120922@163.com>
2020-06-09 11:00:39 +0800

    新21点

--- src/number/New21Game.java
+++ src/number/New21Game.java
@@ -3,16 +3,24 @@ package number;
 /**
  * 描述：837. 新21点
  * N = 21, K = 17, W = 10  0.73278
+ *
  * @author zy
  * @date 2020/6/3 8:31
  */
 public class New21Game {
     public double new21Game(int N, int K, int W) {
-        int totalNum = 0;
-        int vailNum = 0;
-        for (int i = 1; i <= W; i++) {
-
+        if (K == 0) {
+            return 1.0;
         }
-        return totalNum == 0 ? 0 : vailNum / totalNum;
+        double[] dp = new double[K + W];
+        for (int i = K; i <= N && i < K + W; i++) {
+            dp[i] = 1.0;
+        }
+        dp[K - 1] = 1.0 * Math.min(N - K + 1, W) / W;
+        for (int i = K - 2; i >= 0; i--) {
+            dp[i] = dp[i + 1] - (dp[i + W + 1] - dp[i + 1]) / W;
+        }
+        return dp[0];
     }
+
 }
