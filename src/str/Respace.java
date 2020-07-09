e59f71d73e933755ff2afe5b79e6578780f65de6
zy <13216120922@163.com>
2020-07-09 10:54:29 +0800

    恢复空格

---
+++ src/str/Respace.java
@@ -0,0 +1,71 @@
+package str;
+
+import java.util.Arrays;
+
+/**
+ * 描述：面试题 17.13. 恢复空格
+ *
+ * @author zy
+ * @date 2020/7/9 8:52
+ */
+public class Respace {
+
+    public static int respace(String[] dictionary, String sentence) {
+        int n = sentence.length();
+
+        Trie root = new Trie();
+        for (String word : dictionary) {
+            root.insert(word);
+        }
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            dp[i] = dp[i - 1] + 1;
+
+            Trie curPos = root;
+            for (int j = i; j >= 1; --j) {
+                int t = sentence.charAt(j - 1) - 'a';
+                if (curPos.next[t] == null) {
+                    break;
+                } else if (curPos.next[t].isEnd) {
+                    dp[i] = Math.min(dp[i], dp[j - 1]);
+                }
+                if (dp[i] == 0) {
+                    break;
+                }
+                curPos = curPos.next[t];
+            }
+        }
+        return dp[n];
+    }
+
+    static class Trie {
+
+        public Trie[] next;
+        public boolean isEnd;
+
+        public Trie() {
+            next = new Trie[26];
+            isEnd = false;
+        }
+
+        public void insert(String s) {
+            Trie curPos = this;
+
+            for (int i = s.length() - 1; i >= 0; --i) {
+                int t = s.charAt(i) - 'a';
+                if (curPos.next[t] == null) {
+                    curPos.next[t] = new Trie();
+                }
+                curPos = curPos.next[t];
+            }
+            curPos.isEnd = true;
+        }
+    }
+
+    public static void main(String[] args) {
+        respace(new String[]{"looked", "just", "like", "her", "brother"}, "jesslookedjustliketimherbrother");
+    }
+}
