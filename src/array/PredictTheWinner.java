9d9fca68d2e65662fbcb6724f7c29eddfb1edc2a
zy <zhangying@nnuo.com>
2020-09-16 10:41:10 +0800

    预测赢家

--- src/array/PredictTheWinner.java
+++ src/array/PredictTheWinner.java
@@ -2,21 +2,30 @@ package array;
 
 /**
  * 描述：486. 预测赢家
- *  todo:
+ *
  * @author Zhangying
  * @date 2020/9/1 9:06
  */
 public class PredictTheWinner {
 
     public boolean PredictTheWinner(int[] nums) {
-
-       return false;
+        return getResult(nums, 0, nums.length - 1, 0, 0);
     }
 
-    private int getResult(int[] nums, int i, int j) {
-        if (i < j) {
-
+    private boolean getResult(int[] nums, int i, int j, int sum1, int sum2) {
+        if (i > j) {
+            return sum1 >= sum2;
+        } else if (i == j) {
+            return getResult(nums, i + 1, j - 1, sum1 + nums[i], sum2);
+        } else {
+            boolean flag1 = getResult(nums, i + 1, j - 1, sum1 + nums[i], sum2 + nums[j]) && getResult(nums, i + 2, j, sum1 + nums[i], sum2 + nums[i + 1]);
+            boolean flag2 = getResult(nums, i + 1, j - 1, sum1 + nums[j], sum2 + nums[i]) && getResult(nums, i, j - 2, sum1 + nums[j], sum2 + nums[j - 1]);
+            return flag1 || flag2;
         }
-        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new PredictTheWinner().PredictTheWinner(new int[]{1, 5, 2}));
+        System.out.println(new PredictTheWinner().PredictTheWinner(new int[]{1, 5, 233, 7}));
     }
 }
